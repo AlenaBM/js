9acec62df963fc20169e4ef83bc9f93f24f7cb78
AlenaBM <9642444600a@gmail.com>
2021-06-06 19:38:40 +0300

    part-12

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
+'use strict';
 // let unNamed = 390;
 // console.log(unNamed);
 
-const arr =[];
+// const arr =[];
 
-arr[0] = alert('are you gay?');
-arr[1] = confirm('suck some dick?');
-arr[2] = prompt('How many slaves you have?', '');
+// arr[0] = alert('are you gay?');
+// arr[1] = confirm('suck some dick?');
+// arr[2] = prompt('what is your name, master?', '');
 
-console.log(arr);
+// console.log(arr);
 
-if (arr) {
-  document.querySelector('paraf').innerHTML = `<b>${arr}</b>, you are a new master`;}
-  else {
-    document.querySelector('paraf').innerHTML = '<b>Unde</b>, you are a new master';}
+// if (arr[2]) {
+//   document.querySelector('.paraf').innerHTML = `<b>${arr[2]}</b>, you are a new master`;}
+//   else {
+//     alert(typeof(arr[2]));
+//   }
+
+
+// console.log(typeof(null));
+
+// console.log(50 % 500);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
