8ada53edd6a00c9841cf718939160c347fcb86ed
AlenaBM <9642444600a@gmail.com>
2021-06-20 23:45:16 +0300

    functions for genres and check

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 'use strict';
-// let unNamed = 390;
-// console.log(unNamed);
 
-// const arr =[];
-
-// arr[0] = alert('are you gay?');
-// arr[1] = confirm('suck some dick?');
-// arr[2] = prompt('what is your name, master?', '');
-
-// console.log(arr);
-
-// if (arr[2]) {
-//   document.querySelector('.paraf').innerHTML = `<b>${arr[2]}</b>, you are a new master`;}
-//   else {
-//     alert(typeof(arr[2]));
-//   }
-
-
-// console.log(typeof(null));
-
-// console.log(50 % 500);
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -31,12 +10,63 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+function showMyDB(hidden){
+  if (!hidden){
+    console.log(personalMovieDB);
+  } else {
+    console.log('fuck you');
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+showMyDB(personalMovieDB.privat);
 
-console.log(personalMovieDB);
+function writeYourGenres(){
+  for (let i = 1; i<=3; i++){
+  personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером #${i}`);
+}
+}
+
+writeYourGenres();
+
+function start(){
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null ||isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+start();
+
+function rememberMyFilms(){
+  for (let i=0; i<2; i++){
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+  if (a != null && b != null && a !='' && b !='' && a.length < 50){
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel(){
+  if (numberOfFilms < 10){
+  console.log("you've watched very few movies");
+} else if (numberOfFilms >= 10 && numberOfFilms <30) {
+ console.log("you're classic slave");
+} else if (numberOfFilms >= 30) {
+  console.log("you're danger master");
+} else {
+  console.log("it's an error");
+}
+}
+
+detectPersonalLevel();
+
+
+
+
